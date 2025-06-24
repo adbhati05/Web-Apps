53de49c1473904d053adc4e373b1f34699494d2c
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-06-24 05:44:16 -0500

    Structuring the home page by creating a container. Next step is to figure out how to make the container span the entire width of the page, and then resizing it, and then finally putting each paragraph in their correct spots for page structuring purposes.

--- CRUDApp/src/pages/Home.tsx
+++ CRUDApp/src/pages/Home.tsx
@@ -2,10 +2,11 @@ import TopBar from '../components/TopBar';
 import SideBar from '../components/LeftSideBar';
 import PostFeed from '../components/PostFeed';
 import PostCard from '../components/PostCard';
+import './Home.css';
 
 function Home() {
     return (
-        <div>
+        <div className="container">
             <TopBar />
             <SideBar />
             <PostFeed />
