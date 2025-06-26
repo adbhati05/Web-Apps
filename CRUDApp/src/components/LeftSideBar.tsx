40c64c9577c47b5e20c9566ae6310f77e0015b1d
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-06-26 05:38:43 -0500

    Added some buttons to the left side bar and made them match the site aesthetically.

--- CRUDApp/src/components/LeftSideBar.tsx
+++ CRUDApp/src/components/LeftSideBar.tsx
@@ -2,7 +2,17 @@ import './LeftSideBar.css';
 
 function LeftSideBar() {
     return (
-        <div className=''>LeftSideBar</div>
+        // Here I've set up a container for the buttons in the side bar that is a flex box that causes the buttons to stack vertically, not horizontally stretch, and be sized to their contents.
+        // I've also included some horizontal lines to separate the buttons/or links to other pages. 
+        <div className='d-flex flex-column align-items-start spacing'>
+            <button className='left-side-bar-nav'>Home</button>
+            <hr className='left-side-bar-line'/>
+            <button className='left-side-bar-nav'>Style Board</button>
+            <hr className='left-side-bar-line'/>
+            <button className='left-side-bar-nav'>Saved</button>
+            <hr className='left-side-bar-line'/>
+        </div>
+
     );
 }
 
