b0914bd4b31f4a1bd6252a709d7cb5dba5c5114d
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-06-27 20:46:04 -0500

    Edited the top bar of the site by having its name in the top left and the pfp dropdown button in the top right.

--- CRUDApp/src/components/TopBar.tsx
+++ CRUDApp/src/components/TopBar.tsx
@@ -1,8 +1,24 @@
 import './TopBar.css';
+import pfp_placeholder from '../assets/pfp_placeholder.png'; 
+// import test_img from '../assets/test_img.png'; // (when using an image other than the placeholder, the image gets distorted, figure out how to fix that)
 
 function TopBar() {
     return (
-        <div className=''>TopBar</div>
+        <div className='top-bar-content'>FitLog
+            {/* This is the code for the profile picture button that, when clicked, will expand a dropdown that will allow the user to access Settings or Log out. */}
+            <div className='mt-auto profile-dropdown'>
+                <button className='pfp-button'>
+                    <img src={pfp_placeholder} alt='Profile' className='pfp' />
+                    <span className='username'>John Doe</span> {/* Set username character limit to 12 characters */}
+                </button>
+            </div>
+        
+            {/* Do more research on how to incorporate the dropdown element when the user presses on the pfp button */}
+            {/* <div className='dropdown-content'>
+                <a href=''>Settings</a>
+                <a href=''>Log out</a>
+            </div> */}
+        </div>
     );
 }
 
