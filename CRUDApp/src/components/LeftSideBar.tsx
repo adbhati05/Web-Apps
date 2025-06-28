4a380428b85c8490f3523dcf39946ba5bdef4730
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-06-27 20:44:57 -0500

    Removed TSX/CSS code needed for pfp dropdown button so that it can be moved to the TopBar files.

--- CRUDApp/src/components/LeftSideBar.tsx
+++ CRUDApp/src/components/LeftSideBar.tsx
@@ -1,6 +1,4 @@
 import './LeftSideBar.css';
-import pfp_placeholder from '../assets/pfp_placeholder.png';
-// import test_img from '../assets/test_img.png'; (when using an image other than the placeholder, the image gets distorted, figure out how to fix that)
 
 function LeftSideBar() {
     return (
@@ -13,23 +11,7 @@ function LeftSideBar() {
             <hr className='left-side-bar-line'/>
             <button className='left-side-bar-nav'>Saved</button>
             <hr className='left-side-bar-line'/>
-            
-            {/* This is the code for the profile picture button that, when clicked, will expand a dropdown that will allow the user to access Settings or Log out. */}
-            <div className='mt-auto profile-dropdown'>
-                <button className='pfp-button'>
-                    <img src={pfp_placeholder} alt='Profile' className='pfp' />
-                    <span className='username'>John Doe</span>
-                </button>
-            </div>
-
-            {/* Do more research on how to incorporate the dropdown element when the user presses on the pfp button */}
-            {/* <div className='dropdown-content'>
-                <a href=''>Settings</a>
-                <a href=''>Log out</a>
-            </div> */}
-
         </div>
-
     );
 }
 
