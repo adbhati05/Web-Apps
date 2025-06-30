7c3fcac050e06fd19f3dcf6c9694442cce371e66
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-06-30 06:59:15 -0500

    Wrote the foundational code for the login page (created the container that will hold the login card content).

---
+++ CRUDApp/src/pages/Login.tsx
@@ -0,0 +1,10 @@
+import LoginCard from '../components/LoginCard';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className='login-layout'><LoginCard /></div>
+    );
+}
+
+export default Login;
