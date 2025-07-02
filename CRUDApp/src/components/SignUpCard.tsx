7ba60282cd0f23ed5b929b9a0c8378756d3c27bf
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-07-02 07:58:07 -0500

    Designed the sign up card with 4 input fields (username, email, password, confirm password). Also included password rules and obviously sign up button.

--- CRUDApp/src/components/SignUpCard.tsx
+++ CRUDApp/src/components/SignUpCard.tsx
@@ -2,7 +2,36 @@ import './SignUpCard.css';
 
 function SignUpCard() {
     return (
-        <div className=''>Sign Up Card</div>
+        // Include a username input field, an email input field, and a password/confirm password input field (in that order). Be sure to include password rules as well as a paragraph.
+        // Password rules: At least 8 characters, at least one uppercase and lowercase letter, at least one number, and at least one special character.
+        <div className='sign-up-card-content'>Create Account
+            {/* This form is for the four bars in which the user will input their username, email, password, and confirm password. */}
+            <form className='sign-up-input-content'>
+                <label className='sign-up-input-label' htmlFor='Username'>Username:</label>
+                <input className='sign-up-input-bar' type='text' id=''/>
+
+                <label className='sign-up-input-label' htmlFor='Email'>Email:</label>
+                <input className='sign-up-input-bar' type='text' id=''/>
+
+                <label className='sign-up-input-label' htmlFor='Password'>Password:</label>
+                <input className='sign-up-input-bar' type='password' id=''/>
+
+                <label className='sign-up-input-label' htmlFor='ConfirmPassword'>Confirm Password:</label>
+                <input className='sign-up-input-bar' type='password' id=''/>
+            </form>
+
+            {/* This is the unordered list that contains the password rules. */}
+            <ul className='password-rules'>
+                <li>Must be at least 8 characters long.</li>
+                <li>Must contain at least one uppercase letter.</li>
+                <li>Must contain at least one lowercase letter.</li>
+                <li>Must contain at least one number.</li>
+                <li>Must contain at least one special character.</li>
+            </ul>
+
+            {/* This is the create account button that will submit the form, create the account, and forward the user to the log in page to use their new credentials. */}
+            <button className='sign-up-button' type='submit'>Sign up</button>
+        </div>
     );
 }
 
