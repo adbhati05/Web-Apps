e3fa367fa284bc6f709f56268e7f33f05a7336c8
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-07-09 06:02:22 -0500

    Signature for main App.tsx file was also changed.

--- CRUDApp/src/App.tsx
+++ CRUDApp/src/App.tsx
@@ -1,10 +1,17 @@
 import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import './App.css';
 
-function App() {
+const App: React.FunctionComponent = () => {
   return (
+    // Once you have the backend logic for logging in and setting up accounts finished, figure out how to use routing to have the user start on the log in page and then, after signing, navigate to the homepage.
+    // Bear in mind that if the user is on the login page and doesn't enter any credentials, they should not be redirected if the press the button to go to the home page.
+    // Same applies for the sign up page (which redirects the user to the login page after a successful sign up).
     <div>
-      <Home />
+      {/* <Home /> (Commenting this out for now until login page is finished. */}
+      <Login /> {/* (Commenting this out for now until sign-up page is finished. */}
+      {/* <SignUp /> */}
     </div>
   );
 }
