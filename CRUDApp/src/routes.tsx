86136a3266a29a6d8d4061cbfca17c0adea16083
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-07-10 21:52:53 -0500

    Added ProtectedRoutes as a path in routes.tsx so that the home page can only be accessed via login.

--- CRUDApp/src/routes.tsx
+++ CRUDApp/src/routes.tsx
@@ -3,9 +3,21 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
+import ProtectedRoutes from "./auth/ProtectedRoutes";
 
 // Setting up the router for the application. It will contain the routes/or paths for different pages (home, login, sign-up, etc.).
+// Here I've set up an object in which the main element is the ProtectedRoutes component and all of its children are the routes to pages that will be behind the authentication (home page, style board, etc.).
 export const router = createBrowserRouter([
+  {
+    element: <ProtectedRoutes />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: <Error />
+      }
+    ]
+  },
   {
     path: "/login",
     element: <Login />,
@@ -16,11 +28,6 @@ export const router = createBrowserRouter([
     element: <SignUp />,
     errorElement: <Error />
   },
-  { 
-    path: "/home",
-    element: <Home />,
-    errorElement: <Error />
-  }
 ]);
 
 export default router;
