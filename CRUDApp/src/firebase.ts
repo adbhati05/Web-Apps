b3bad13bb9dfb86f2d36ddf8be98d9658f920b6d
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-07-12 07:07:36 -0500

    Replaced process.env with import.meta.env for each of the keys since that's the correct syntax.

--- CRUDApp/src/firebase.ts
+++ CRUDApp/src/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage"; // This will allow me to have sto
 
 // Initializing the Firebase configuration object with all the details from the project overview.
 const firebaseConfig = {
-    apiKey: process.env.VITE_FIREBASE_API_KEY,
-    authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.VITE_FIREBASE_APP_ID,
-    measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initializing Firebase by passing in the const above into initializeApp.
