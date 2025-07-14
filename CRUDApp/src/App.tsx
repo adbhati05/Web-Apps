dff573f4ab6ed91c938f5a63e67fa1cb02998fc0
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-07-14 05:50:15 -0500

    Edited this file to have the RouterProvider be wrapped around UserAuthProvider.

--- CRUDApp/src/App.tsx
+++ CRUDApp/src/App.tsx
@@ -1,18 +1,17 @@
-import Home from './pages/Home';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+import { RouterProvider } from 'react-router-dom';
+import { UserAuthProvider } from './auth/UserAuthContext';
+import router from './routes';
+
 import './App.css';
 
-const App: React.FunctionComponent = () => {
+const App = () => {
   return (
     // Once you have the backend logic for logging in and setting up accounts finished, figure out how to use routing to have the user start on the log in page and then, after signing, navigate to the homepage.
     // Bear in mind that if the user is on the login page and doesn't enter any credentials, they should not be redirected if the press the button to go to the home page.
     // Same applies for the sign up page (which redirects the user to the login page after a successful sign up).
-    <div>
-      {/* <Home /> (Commenting this out for now until login page is finished. */}
-      <Login /> {/* (Commenting this out for now until sign-up page is finished. */}
-      {/* <SignUp /> */}
-    </div>
+    <UserAuthProvider>
+      <RouterProvider router={router} />
+    </UserAuthProvider>
   );
 }
 
