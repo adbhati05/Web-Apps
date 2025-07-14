d59312459b05ef258ef9250cb78ee99039c15179
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-07-14 05:59:57 -0500

    Initializing the files for the settings page.

---
+++ CRUDApp/src/pages/Settings.tsx
@@ -0,0 +1,12 @@
+import './Settings.css'
+
+const Settings = () => {
+    return (
+        <div className='settings-layout'>
+            Settings Page
+            {/* Add your settings components here */}
+        </div>
+    );
+}
+
+export default Settings;
