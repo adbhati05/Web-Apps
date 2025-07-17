8a024677a1884a2e02329374cfd8746d019dc0c9
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-07-16 20:01:54 -0500

    Edited the elements in the left side bar to be links instead of buttons (also added the link to the Post page).

--- CRUDApp/src/components/LeftSideBar.tsx
+++ CRUDApp/src/components/LeftSideBar.tsx
@@ -1,15 +1,18 @@
 import './LeftSideBar.css';
+import { Link } from 'react-router-dom';
 
 const LeftSideBar = () => {
     return (
         // Here I've set up a container for the buttons in the side bar that is a flex box that causes the buttons to stack vertically, not horizontally stretch, and be sized to their contents.
         // I've also included some horizontal lines to separate the buttons/or links to other pages. 
         <div className='d-flex flex-column align-items-start spacing'>
-            <button className='left-side-bar-button'>Home</button> 
+            <Link to='/' className='left-side-bar-button'>Home</Link>
             <hr className='left-side-bar-line'/> 
-            <button className='left-side-bar-button'>Style Board</button>
+            <Link to='/styleboard' className='left-side-bar-button'>Style Board</Link>
             <hr className='left-side-bar-line'/>
-            <button className='left-side-bar-button'>Saved</button>
+            <Link to='/saved' className='left-side-bar-button'>Saved</Link>
+            <hr className='left-side-bar-line'/>
+            <Link to='/post' className='left-side-bar-button'>Post</Link>
             <hr className='left-side-bar-line'/>
         </div>
     );
