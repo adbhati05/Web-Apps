da037c37fb3ef358eac17ef1459080a88768694c
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-08-06 22:13:29 -0500

    Reformatted the TSX files to look cleaner, also changed up styling of these components a tiny bit.

--- CRUDApp/src/components/SignUpCard.tsx
+++ CRUDApp/src/components/SignUpCard.tsx
@@ -1,150 +1,205 @@
-import './SignUpCard.css';
-import { useState } from 'react';
-import type { ChangeEvent, FormEvent } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useUserAuth } from '../auth/UserAuthContext';
-import { authService } from '../services/auth.service';
-import { Link } from 'react-router-dom';
+import "./SignUpCard.css";
+import { useState } from "react";
+import type { ChangeEvent, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUserAuth } from "../auth/UserAuthContext";
+import { authService } from "../services/auth.service";
+import { Link } from "react-router-dom";
 
 // NOTE: More validation/error-handling code will need to be implemented. DO RESEARCH!
 const SignUpCard = () => {
-    // Setting up the userSignUpInfo state object to hold the user's sign-up information.
-    const [userSignUpInfo, setUserSignUpInfo] = useState({
-        email: '',
-        username: '',
-        password: '',
-        confirmPassword: ''
-    });
+  // Setting up the userSignUpInfo state object to hold the user's sign-up information.
+  const [userSignUpInfo, setUserSignUpInfo] = useState({
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-    // Setting up useNavigate hook to navigate user to the home page after successful sign-up.
-    const navigate = useNavigate();
+  // Setting up useNavigate hook to navigate user to the home page after successful sign-up.
+  const navigate = useNavigate();
 
-    // Importing the signUp function from UserAuthContext to handle user registration.
-    const { signUp } = useUserAuth();
+  // Importing the signUp function from UserAuthContext to handle user registration.
+  const { signUp } = useUserAuth();
 
-    // This function uses the id attribute of the input field it's located in to update the userInfo state object with the value entered by the user via setUserInfo.
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const { id, value } = e.target;
-        setUserSignUpInfo({...userSignUpInfo, [id]: value});
-    };
+  // This function uses the id attribute of the input field it's located in to update the userInfo state object with the value entered by the user via setUserInfo.
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setUserSignUpInfo({ ...userSignUpInfo, [id]: value });
+  };
 
-    // This function ensures that when the user presses the "Sign up" button, the form will not perform its default action (page refresh) and will instead call signUp with the appropriate args and forward the user to the home page.
-    // Note: In future, include email verification before allowing the user to log in from sign up (look into this).
-    const handleSubmit = async(e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  // This function ensures that when the user presses the "Sign up" button, the form will not perform its default action (page refresh) and will instead call signUp with the appropriate args and forward the user to the home page.
+  // Note: In future, include email verification before allowing the user to log in from sign up (look into this).
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        // Checking if username is valid by setting up a regex that ensures it can only contain A-z, a-z, 0-9, and underscores.
-        const usernameRegex = /^[A-Za-z0-9_]+$/;
-        if (!usernameRegex.test(userSignUpInfo.username)) {
-            alert("Username can only contain letters, numbers, and underscores.");
-            return;
-        }
-
-        // Checking if username is available via checkUsernameExists from authService.
-        const isUsernameAvailable = await authService.checkUsernameExists(userSignUpInfo.username);
-        if (isUsernameAvailable) {
-            alert("Username is already taken.");
-            return;
-        }
-
-        // First ensuring password is long enough before proceeding with other checks.
-        if (userSignUpInfo.password.length < 8) {
-            alert("Password must be at least 8 characters long.");
-            return;
-        } else {
-            // Left out `, \, /, ', ", <, and > since these speial characters can cause issues if input is not properly sanitized, leading to XSS or SQL injection attacks.
-            // Setting up a constant for special characters check.
-            const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', ':', ';', ',', '.', '?', '~', '|'];
-            const hasSpecialChar = specialChars.some(char => userSignUpInfo.password.includes(char));
-
-            // Setting up constants for uppercase, lowercase, and number checks.
-            const hasUpperCase = /[A-Z]/.test(userSignUpInfo.password);
-            const hasLowerCase = /[a-z]/.test(userSignUpInfo.password);
-            const hasNumber = /\d/.test(userSignUpInfo.password);
-
-            if (!hasSpecialChar) {
-                alert("Password must contain at least one special character.");
-                return;
-            }
-
-            if (!hasUpperCase) {
-                alert("Password must contain at least one uppercase letter.");
-                return;
-            }
-
-            if (!hasLowerCase) {
-                alert("Password must contain at least one lowercase letter.");
-                return;
-            }
-
-            if (!hasNumber) {
-                alert("Password must contain at least one number.");
-                return;
-            }
-        }
-
-        // Ensure passwords match before proceeding with sign up.
-        if (userSignUpInfo.password !== userSignUpInfo.confirmPassword) {
-            alert("Passwords must match.");
-            return;
-        }
-
-        try {
-            // Logging user info to console for debugging purposes. Will not be in production.
-            console.log("The user info is: ", userSignUpInfo);
-
-            // Since both displayName and username depend on the input from the user for the Username field in the sign-up card, userSignUpInfo.username is passed as both displayName and username to signUp.
-            await signUp(userSignUpInfo.email, userSignUpInfo.password, userSignUpInfo.username, userSignUpInfo.username);
-            navigate("/");
-        } catch (error) {
-            console.log("An error occurred while signing up: ", error);
-        }
+    // Checking if username is valid by setting up a regex that ensures it can only contain A-z, a-z, 0-9, and underscores.
+    const usernameRegex = /^[A-Za-z0-9_]+$/;
+    if (!usernameRegex.test(userSignUpInfo.username)) {
+      alert("Username can only contain letters, numbers, and underscores.");
+      return;
     }
 
-    return (
-        // Below is a username input field, an email input field, password/confirm password input field (in that order, a set of password rules, and a submit button. 
-        // Password rules: At least 8 characters, at least one uppercase and lowercase letter, at least one number, and at least one special character.
-        <div className='sign-up-card-container'>Create Account
-            <form className='sign-up-form-container' onSubmit={handleSubmit}>
-                <div className='sign-up-input-field-container'>
-                    <label className='sign-up-input-label' htmlFor='Username'>Username:</label>
-                    <input className='sign-up-input-bar' id='username'type='username' placeholder='Must be 12 characters max' value={userSignUpInfo.username} onChange={handleChange} required maxLength={12}/>
-                </div>
-
-                <div className='sign-up-input-field-container'>
-                    <label className='sign-up-input-label' htmlFor='Email'>Email:</label> 
-                    <input className='sign-up-input-bar' id='email'type='email' value={userSignUpInfo.email} onChange={handleChange} required/>
-                </div>
-
-                <div className='sign-up-input-field-container'>
-                    <label className='sign-up-input-label' htmlFor='Password'>Password:</label>
-                    <input className='sign-up-input-bar' id='password' type='password' placeholder='See rules below' value={userSignUpInfo.password} onChange={handleChange} required/>
-                </div>
-
-                <div className='sign-up-input-field-container'>
-                    <label className='sign-up-input-label' htmlFor='ConfirmPassword'>Confirm Password:</label>
-                    <input className='sign-up-input-bar' id='confirmPassword' type='password' value={userSignUpInfo.confirmPassword} onChange={handleChange} required/>
-                </div>
-
-                <ul className='password-rules'>
-                    <li>Must be at least 8 characters long.</li>
-                    <li>Must contain at least one uppercase letter.</li>
-                    <li>Must contain at least one lowercase letter.</li>
-                    <li>Must contain at least one number.</li>
-                    <li>Must contain at least one special character.</li>
-                </ul>
-
-                <button className='sign-up-button' type='submit'>Sign up</button>
-
-                {/* Add CSS to make link match with rest of card. */}
-                <div className='login-link-container'>
-                    <p>
-                        Already have an account? Log in <Link className='login-link' to="/login">here.</Link>
-                    </p>
-                </div>
-            </form>
-        </div>
+    // Checking if username is available via checkUsernameExists from authService.
+    const isUsernameAvailable = await authService.checkUsernameExists(
+      userSignUpInfo.username
     );
-}
+    if (isUsernameAvailable) {
+      alert("Username is already taken.");
+      return;
+    }
+
+    // First ensuring password is long enough before proceeding with other checks.
+    if (userSignUpInfo.password.length < 8) {
+      alert("Password must be at least 8 characters long.");
+      return;
+    } else {
+      // Left out `, \, /, ', ", <, and > since these speial characters can cause issues if input is not properly sanitized, leading to XSS or SQL injection attacks.
+      // Setting up a constant for special characters check.
+      const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', ':', ';', ',', '.', '?', '~', '|'];
+      const hasSpecialChar = specialChars.some((char) =>
+        userSignUpInfo.password.includes(char)
+      );
+
+      // Setting up constants for uppercase, lowercase, and number checks.
+      const hasUpperCase = /[A-Z]/.test(userSignUpInfo.password);
+      const hasLowerCase = /[a-z]/.test(userSignUpInfo.password);
+      const hasNumber = /\d/.test(userSignUpInfo.password);
+
+      if (!hasSpecialChar) {
+        alert("Password must contain at least one special character.");
+        return;
+      }
+
+      if (!hasUpperCase) {
+        alert("Password must contain at least one uppercase letter.");
+        return;
+      }
+
+      if (!hasLowerCase) {
+        alert("Password must contain at least one lowercase letter.");
+        return;
+      }
+
+      if (!hasNumber) {
+        alert("Password must contain at least one number.");
+        return;
+      }
+    }
+
+    // Ensure passwords match before proceeding with sign up.
+    if (userSignUpInfo.password !== userSignUpInfo.confirmPassword) {
+      alert("Passwords must match.");
+      return;
+    }
+
+    try {
+      // Logging user info to console for debugging purposes. Will not be in production.
+      console.log("The user info is: ", userSignUpInfo);
+
+      // Since both displayName and username depend on the input from the user for the Username field in the sign-up card, userSignUpInfo.username is passed as both displayName and username to signUp.
+      await signUp(
+        userSignUpInfo.email,
+        userSignUpInfo.password,
+        userSignUpInfo.username,
+        userSignUpInfo.username
+      );
+      navigate("/");
+    } catch (error) {
+      console.log("An error occurred while signing up: ", error);
+    }
+  };
+
+  return (
+    // Below is a username input field, an email input field, password/confirm password input field (in that order, a set of password rules, and a submit button.
+    // Password rules: At least 8 characters, at least one uppercase and lowercase letter, at least one number, and at least one special character.
+    <div className="sign-up-card-container">
+      Create Account
+      <form className="sign-up-form-container" onSubmit={handleSubmit}>
+        <div className="sign-up-input-field-container">
+          <label className="sign-up-input-label" htmlFor="Username">
+            Username:
+          </label>
+          <input
+            className="sign-up-input-bar"
+            id="username"
+            type="username"
+            placeholder="Must be 3-16 characters long"
+            value={userSignUpInfo.username}
+            onChange={handleChange}
+            required
+            minLength={3}
+            maxLength={16}
+          />
+        </div>
+
+        <div className="sign-up-input-field-container">
+          <label className="sign-up-input-label" htmlFor="Email">
+            Email:
+          </label>
+          <input
+            className="sign-up-input-bar"
+            id="email"
+            type="email"
+            value={userSignUpInfo.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="sign-up-input-field-container">
+          <label className="sign-up-input-label" htmlFor="Password">
+            Password:
+          </label>
+          <input
+            className="sign-up-input-bar"
+            id="password"
+            type="password"
+            placeholder="See rules below"
+            value={userSignUpInfo.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div className="sign-up-input-field-container">
+          <label className="sign-up-input-label" htmlFor="ConfirmPassword">
+            Confirm Password:
+          </label>
+          <input
+            className="sign-up-input-bar"
+            id="confirmPassword"
+            type="password"
+            value={userSignUpInfo.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <ul className="password-rules">
+          <li>Must be at least 8 characters long.</li>
+          <li>Must contain at least one uppercase letter.</li>
+          <li>Must contain at least one lowercase letter.</li>
+          <li>Must contain at least one number.</li>
+          <li>Must contain at least one special character.</li>
+        </ul>
+
+        <button className="sign-up-button" type="submit">
+          Sign up
+        </button>
+
+        {/* Add CSS to make link match with rest of card. */}
+        <div className="login-link-container">
+          <p>
+            Already have an account? Log in{" "}
+            <Link className="login-link" to="/login">
+              here.
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SignUpCard;
--- CRUDApp/src/components/TopBar.tsx
+++ CRUDApp/src/components/TopBar.tsx
@@ -1,72 +1,82 @@
-import './TopBar.css';
-import pfp_placeholder from '../assets/pfp_placeholder.png'; 
+import "./TopBar.css";
+import pfp_placeholder from "../assets/pfp_placeholder.png";
 // import test_img from '../assets/test_img.png'; // (when using an image other than the placeholder, the image gets distorted, figure out how to fix that)
-import { useState, useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useUserAuth } from '../auth/UserAuthContext';
-import { useNavigate } from 'react-router-dom';
+import { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useUserAuth } from "../auth/UserAuthContext";
+import { useNavigate } from "react-router-dom";
 
 const TopBar = () => {
-    // Creating a hook in which the state variable is a boolean indicating whether the dropdown is open (true) or closed (false) (setter function obviously updates that state whenever the user clicks on the button).
-    const [open, setOpen] = useState(false);
+  // Creating a hook in which the state variable is a boolean indicating whether the dropdown is open (true) or closed (false) (setter function obviously updates that state whenever the user clicks on the button).
+  const [open, setOpen] = useState(false);
 
-    // Creating a mutable reference object for <div> representing dropdown (AKA profile-dropdown).
-    const dropdownRef = useRef<HTMLDivElement>(null);
+  // Creating a mutable reference object for <div> representing dropdown (AKA profile-dropdown).
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
-    // Using useEffect to handle clicks outside the dropdown, where if it is open it should close.
-    useEffect(() => {
-        // Here I am passing a mouseEvent value into handleClickOutside so that I can have the dropdown close if it's been opened and a click has been registered outside of the dropdown.
-        const handleClickOutside = (e: MouseEvent) => {
-            // If dropdownRef.current exists (meaning that the dropdown has been opened) and the user has clicked anywhere OUTSIDE of the opened dropdown, pass false into setOpen to have the dropdown menu close.
-            if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
-                setOpen(false);
-            }
-        };
- 
-        // Here I am using the document JS object to represent the entire HTML doc loaded in the browser so that we can pass handleClickOutside into an event listener to register clicks anywhere on the document.
-        document.addEventListener('click', handleClickOutside);
-
-        // Final step here is to clean up and remove the event listener when the dropdown re-renders.
-        return () => document.removeEventListener('click', handleClickOutside);
-    }, []);
-
-    // Setting up the log out functionality by leveraging signOut from UserAuthContext.
-    const { signOut } = useUserAuth();
-    const navigate = useNavigate();
-
-    const handleSignOut = async () => {
-        try {
-            await signOut();
-            navigate('/login'); // Redirect to login page ONLY after logOut() is successful in execution.
-        } catch (error) {
-            console.error("An error occurred while logging out: ", error);
-        }
+  // Using useEffect to handle clicks outside the dropdown, where if it is open it should close.
+  useEffect(() => {
+    // Here I am passing a mouseEvent value into handleClickOutside so that I can have the dropdown close if it's been opened and a click has been registered outside of the dropdown.
+    const handleClickOutside = (e: MouseEvent) => {
+      // If dropdownRef.current exists (meaning that the dropdown has been opened) and the user has clicked anywhere OUTSIDE of the opened dropdown, pass false into setOpen to have the dropdown menu close.
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
+        setOpen(false);
+      }
     };
 
-    // Retrieving the current user's info to acquire their displayName and profile picture (future implementation) so that both can be displayed in the pfp dropdown button.
-    const { userInfo } = useUserAuth();
-    const displayName = userInfo?.displayName || 'User'; // Fallback to 'User' if displayName is not available (this will only occur if there's database corruption, manual database edits, etc).
+    // Here I am using the document JS object to represent the entire HTML doc loaded in the browser so that we can pass handleClickOutside into an event listener to register clicks anywhere on the document.
+    document.addEventListener("click", handleClickOutside);
 
-    return (
-        <div className='top-bar-content'>FitLog
-            {/* This is the code for the profile picture button that, when clicked, will expand a dropdown that will allow the user to access Settings or Log out. */}
-            <div className='mt-auto profile-dropdown' ref={dropdownRef}>
-                <button className='pfp-button' onClick={() => {console.log('Dropdown toggled: ', !open); setOpen(!open);}}> {/* Setting open to false to ensure the dropdown is not expanded when the component mounts. */}
-                    <img src={pfp_placeholder} alt='Profile' className='pfp' /> 
-                    <span className='username'>{displayName}</span> {/* Set username character limit to 12 characters */}
-                </button>
+    // Final step here is to clean up and remove the event listener when the dropdown re-renders.
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, []);
 
-                {/* Here the dropdown's content will conditionally render if open is set to true. */}
-                {open && (
-                    <div className='dropdown-content'>
-                        <Link to='/'>Home</Link>
-                        <Link to='/settings'>Settings</Link>
-                        <button onClick={handleSignOut}>Log out</button>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
-}
+  // Setting up the log out functionality by leveraging signOut from UserAuthContext.
+  const { signOut } = useUserAuth();
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      navigate("/login"); // Redirect to login page ONLY after logOut() is successful in execution.
+    } catch (error) {
+      console.error("An error occurred while logging out: ", error);
+    }
+  };
+
+  // Retrieving the current user's info to acquire their displayName and profile picture (future implementation) so that both can be displayed in the pfp dropdown button.
+  const { userInfo } = useUserAuth();
+  const displayName = userInfo?.displayName || "User"; // Fallback to 'User' if displayName is not available (this will only occur if there's database corruption, manual database edits, etc).
+
+  return (
+    <div className="top-bar-content">
+      FitLog
+      {/* This is the code for the profile picture button that, when clicked, will expand a dropdown that will allow the user to access Settings or Log out. */}
+      <div className="mt-auto profile-dropdown" ref={dropdownRef}>
+        <button
+          className="pfp-button"
+          onClick={() => {
+            console.log("Dropdown toggled: ", !open);
+            setOpen(!open);
+          }}
+        >
+          {" "}
+          {/* Setting open to false to ensure the dropdown is not expanded when the component mounts. */}
+          <img src={pfp_placeholder} alt="Profile" className="pfp" />
+          <span className="username">{displayName}</span>{" "}
+          {/* Set username character limit to 12 characters */}
+        </button>
+
+        {/* Here the dropdown's content will conditionally render if open is set to true. */}
+        {open && (
+          <div className="dropdown-content">
+            <Link to="/">Home</Link>
+            <Link to="/settings">Settings</Link>
+            <button onClick={handleSignOut}>Log out</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default TopBar;
