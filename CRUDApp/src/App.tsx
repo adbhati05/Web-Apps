391e36c7f825810b0bf4e630855b1742ad0aafcd
adbhati05 <bhati.18@buckeyemail.osu.edu>
2025-09-17 16:46:04 -0400

    Edited these files to look cleaner.

--- CRUDApp/src/App.tsx
+++ CRUDApp/src/App.tsx
@@ -6,9 +6,6 @@ import './App.css';
 
 const App = () => {
   return (
-    // Once you have the backend logic for logging in and setting up accounts finished, figure out how to use routing to have the user start on the log in page and then, after signing, navigate to the homepage.
-    // Bear in mind that if the user is on the login page and doesn't enter any credentials, they should not be redirected if the press the button to go to the home page.
-    // Same applies for the sign up page (which redirects the user to the login page after a successful sign up).
     <UserAuthProvider>
       <RouterProvider router={router} />
     </UserAuthProvider>
--- CRUDApp/src/main.tsx
+++ CRUDApp/src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css' 
 import App from './App.tsx'
 
-// The reason why 'bootstrap/dist/css/bootstrap.css' is imported here is to ensure that Bootstrap styling is applied globally in the application.
-// This makes it so any component or TSX files I edit will have access to Bootstrap's CSS classes and styles.
-// Make sure to incorporate your own custom CSS styling in case Bootstrap's default styles do not fit your needs.
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
