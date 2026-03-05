42907d1a035cb5275311e11ee28262652832c42e
adbhati05 <bhati.18@buckeyemail.osu.edu>
2026-03-05 15:13:30 -0500

    I forgot to mention, currently working on the post card's layout, so that's why there are placeholders in PostFeed.tsx

--- CRUDApp/src/components/PostFeed.tsx
+++ CRUDApp/src/components/PostFeed.tsx
@@ -1,6 +1,8 @@
 import './PostFeed.css';
 import PostCard from './PostCard';
 
+// For now I'll use placeholders in order to visualize how the card will look since I'm currently working on the post card's aesthetics. 
+// Eventually, this component will fetch all posts from the database and display them in the feed.
 const PostFeed = () => {
     return (
         <div className='post-feed-container'>
